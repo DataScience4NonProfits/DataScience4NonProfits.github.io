e3523ce4421db4124f173a96eb63b333d8e7fe3e
Brett <brett.castellanos@gmail.com>
2019-11-16 15:11:19 -0800

    update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,40 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'DataScience4NonProfits'
-SITENAME = 'DataScience4NonProfits'
-SITEURL = 'https://DataScience4NonProfits.github.io'
+THEME = '/Users/brettcastellanos/projects/pelican-themes/Flex'
 
 PATH = 'content'
-
-TIMEZONE = 'America/Los_Angeles'
+STATIC_PATHS = ['images']
+MARKUP = ('md', 'ipynb')
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
+IGNORE_FILES = [".ipynb_checkpoints"]
 
 DEFAULT_LANG = 'en'
+TIMEZONE = 'America/Los_Angeles'
+
+AUTHOR = 'DataScience4NonProfits'
+SITEURL = 'https://DataScience4NonProfits.github.io'
+SITENAME = 'DataScience4NonProfits'
+SITE_TITLE = 'Data Science for Non-Profits'
+SITE_SUBTITLE = 'San Diego, CA'
+SITELOGO = SITEURL + '/images/profile.png'
+FAVICON = SITEURL + '/images/favicon.ico'
+
+BROWSER_COLOR = '#333'
+ROBOTS = 'index, follow'
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-ds4nonprofits'
+}
+COPYRIGHT_YEAR = 2019
+
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+}
+CUSTOM_CSS = 'static/custom.css'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,6 +44,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+MAIN_MENU = True
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
@@ -26,8 +53,8 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('meetup', 'https://www.meetup.com/Data-Science-for-Non-Profits/'),)
+)
 
 DEFAULT_PAGINATION = 10
 
